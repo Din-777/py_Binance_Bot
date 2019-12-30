a6f995c1e801fb620c3f4930b6e3cf770da59405
Din <din_777@mail.ru>
2019-12-30 22:36:14 +0500

    telegram get_messeges

--- Binance_testBot/Temp.py
+++ Binance_testBot/Temp.py
@@ -16,4 +16,21 @@ keys = API_keys("../keys.txt")
 client = TelegramClient('session_name', keys.tl_id, keys.tl_sec)
 client.start()
 
+username = 'CQSScalpingFree'
+dp = client.get_entity(username)
+
+messages = client.get_messages(username, limit=10000)
+
+msg = {}
+for m in messages:
+    msg[m.date] = m.message
+
+import pickle
+with open('..\CQS_10000msg_301219.pickle', 'wb') as f:
+    pickle.dump(msg, f)
+
+with open('..\CQS_10000msg_301219.pickle', 'rb') as f:
+    data_new = pickle.load(f)
+
+
 print('saf')
